7ebeb231ed534fba533a5ae897dba1b655c8cdf7
Piyush Suthar <piyushsuthar100@gmail.com>
2023-07-20 01:27:26 +0530

    added props to card component

--- lib/cards.dart
+++ lib/cards.dart
@@ -3,35 +3,47 @@ import 'package:flutter/material.dart';
 class NewsCard extends StatelessWidget {
   const NewsCard({
     super.key,
+    required this.title,
+    required this.subtitle,
+    required this.image,
   });
+
+  final String title;
+  final String subtitle;
+  final String image;
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(10.0),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
       child: Card(
         borderOnForeground: true,
-        shape: RoundedRectangleBorder(
+        shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(20.0))),
         child: Column(
           children: <Widget>[
             ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              borderRadius: const BorderRadius.all(Radius.circular(20.0)),
               child: Image(
-                image: NetworkImage(
-                    "https://i0.wp.com/spacenews.com/wp-content/uploads/2023/07/hsc_20191119-33-scaled.jpg"),
+                image: NetworkImage(image),
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(
+              padding: const EdgeInsets.only(
                   top: 15.0, bottom: 20.0, left: 10.0, right: 10.0),
               child: ListTile(
                 title: Text(
-                  "Ancient River Is Helping NASA's Perseverance Mars Rover Do Its Work",
-                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17.0),
+                  title,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w600, fontSize: 17.0),
                 ),
-                subtitle: Text(
-                    "The six-wheeled geologist is getting some assistance in its in search for diverse rock samples that could be brought to Earth for deeper investigation."),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(
+                    subtitle,
+                    style: const TextStyle(fontSize: 15.0),
+                  ),
+                ),
               ),
             )
           ],
