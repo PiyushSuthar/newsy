b98f8f5c7b8eb9c25ca3927689dcfdd72da2aa16
Piyush Suthar <piyushsuthar100@gmail.com>
2023-07-20 15:30:00 +0530

    added bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -47,13 +47,14 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _getNews() async {
-    _news = (await NewsApi.getNews())!;
+    _news = (await NewsApi.getNews());
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
         // change brightnes of the background color
         appBar: AppBar(
           title: const Text(
@@ -68,16 +69,63 @@ class _HomePageState extends State<HomePage> {
             ),
             IconButton(
               icon: const Icon(Icons.settings),
-              onPressed: () {},
+              onPressed: () {
+                showModalBottomSheet<void>(
+                  context: context,
+                  showDragHandle: true,
+                  builder: (context) {
+                    return const SizedBox(
+                      height: 300,
+                      child: Center(
+                        child: Text("Settings"),
+                      ),
+                    );
+                  },
+                );
+              },
             ),
           ],
         ),
-        body: ListView(
-            children: _news
-                .map((news) => NewsCard(
-                    title: news.title,
-                    subtitle: news.summary,
-                    image: news.imageUrl))
-                .toList()));
+        bottomNavigationBar: NavigationBar(
+          destinations: const <NavigationDestination>[
+            NavigationDestination(
+              selectedIcon: Icon(Icons.newspaper),
+              icon: Icon(Icons.newspaper_outlined),
+              label: 'News',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.bookmark),
+              icon: Icon(Icons.bookmark_border),
+              label: 'Saved',
+            ),
+          ],
+        ),
+        body: _news.isEmpty
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView(
+                children: _news
+                    .map((news) => NewsCard(
+                        title: news.title.toString(),
+                        subtitle: news.summary.toString(),
+                        image: news.imageUrl))
+                    .toList()));
+  }
+}
+
+class SettingsRoute extends StatelessWidget {
+  const SettingsRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: const Center(
+        child: Text("Settings"),
+      ),
+    );
   }
 }
